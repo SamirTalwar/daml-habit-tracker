44733303805abcbbe02b04f3d5adab5fa87b896a
Samir Talwar <samir@noodlesandwich.com>
2020-06-26 16:21:32 +0200

    Make a list of habits, and let you add a new one.

---
+++ ui/src/components/HabitList.tsx
@@ -0,0 +1,62 @@
+// Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from "react";
+import {Form, List, Button} from "semantic-ui-react";
+import {Model} from "@daml.js/create-daml-app";
+
+type Props = {
+  habits: Model.Habit[];
+  onAddHabit: (name: string) => Promise<void>;
+};
+
+/**
+ * React component to edit a list of `Party`s.
+ */
+const HabitList: React.FC<Props> = ({habits, onAddHabit}) => {
+  const [newHabit, setNewHabit] = React.useState("");
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  const addParty = async (event?: React.FormEvent) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setIsSubmitting(true);
+    try {
+      await onAddHabit(newHabit);
+      setIsSubmitting(false);
+      setNewHabit("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <List relaxed>
+      {[...habits]
+        .sort((x, y) => x.name.localeCompare(y.name))
+        .map((habit) => (
+          <List.Item key={habit.name}>
+            <List.Icon name="arrow circle right" />
+            <List.Content>
+              <List.Header>{habit.name}</List.Header>
+            </List.Content>
+          </List.Item>
+        ))}
+      <br />
+      <Form onSubmit={addParty}>
+        <Form.Input
+          fluid
+          readOnly={isSubmitting}
+          loading={isSubmitting}
+          placeholder="Exercise, cook, meditate, learn…"
+          value={newHabit}
+          onChange={(event) => setNewHabit(event.currentTarget.value)}
+        />
+        <Button type="submit">Make it</Button>
+      </Form>
+    </List>
+  );
+};
+
+export default HabitList;
--- ui/src/components/MainView.tsx
+++ ui/src/components/MainView.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, {useMemo} from "react";
+import React from "react";
 import {
   Container,
   Grid,
@@ -10,50 +10,19 @@ import {
   Segment,
   Divider,
 } from "semantic-ui-react";
-import {Party} from "@daml/types";
-import {User} from "@daml.js/create-daml-app";
-import {
-  useParty,
-  useLedger,
-  useStreamFetchByKey,
-  useStreamQuery,
-} from "@daml/react";
-import UserList from "./UserList";
-import PartyListEdit from "./PartyListEdit";
+import {Model} from "@daml.js/create-daml-app";
+import {useParty, useLedger, useStreamQuery} from "@daml/react";
+import HabitList from "./HabitList";
 
-// USERS_BEGIN
 const MainView: React.FC = () => {
-  const username = useParty();
-  const myUserResult = useStreamFetchByKey(User.User, () => username, [
-    username,
-  ]);
-  const myUser = myUserResult.contract?.payload;
-  const allUsers = useStreamQuery(User.User).contracts;
-  // USERS_END
-
-  // Sorted list of users that are following the current user
-  const followers = useMemo(
-    () =>
-      allUsers
-        .map((user) => user.payload)
-        .filter((user) => user.username !== username)
-        .sort((x, y) => x.username.localeCompare(y.username)),
-    [allUsers, username],
-  );
-
-  // FOLLOW_BEGIN
   const ledger = useLedger();
+  const username = useParty();
+  const habits = useStreamQuery(Model.Habit).contracts;
 
-  const follow = async (userToFollow: Party): Promise<boolean> => {
-    try {
-      await ledger.exerciseByKey(User.User.Follow, username, {userToFollow});
-      return true;
-    } catch (error) {
-      alert("Unknown error:\n" + JSON.stringify(error));
-      return false;
-    }
-  };
-  // FOLLOW_END
+  const newHabit = (name: string): Promise<void> =>
+    ledger
+      .create(Model.Habit, {owner: username, keepingHonest: null, name})
+      .then(() => {});
 
   return (
     <Container>
@@ -67,37 +36,19 @@ const MainView: React.FC = () => {
               textAlign="center"
               style={{padding: "1ex 0em 0ex 0em"}}
             >
-              {myUser ? `Welcome, ${myUser.username}!` : "Loading..."}
+              Habits
             </Header>
 
             <Segment>
               <Header as="h2">
-                <Icon name="user" />
-                <Header.Content>
-                  {myUser?.username ?? "Loading..."}
-                  <Header.Subheader>Users I'm following</Header.Subheader>
-                </Header.Content>
+                <Icon name="list" />
+                <Header.Content>Habits</Header.Content>
               </Header>
               <Divider />
-              <PartyListEdit
-                parties={myUser?.following ?? []}
-                onAddParty={follow}
+              <HabitList
+                habits={habits.map((habit) => habit.payload)}
+                onAddHabit={newHabit}
               />
-            </Segment>
-            <Segment>
-              <Header as="h2">
-                <Icon name="globe" />
-                <Header.Content>
-                  The Network
-                  <Header.Subheader>
-                    My followers and users they are following
-                  </Header.Subheader>
-                </Header.Content>
-              </Header>
-              <Divider />
-              {/* USERLIST_BEGIN */}
-              <UserList users={followers} onFollow={follow} />
-              {/* USERLIST_END */}
             </Segment>
           </Grid.Column>
         </Grid.Row>
